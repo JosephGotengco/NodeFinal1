c8b01467e2f21c722bc2834aed15143b0505dd6e
Standard <josephgotnco@gmail.com>
2019-04-16 19:53:31 -0700

    Updated index.hbs and server.js

--- server.js
+++ server.js
@@ -13,74 +13,15 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get('/', (request, response) => {
-    response.render('index.hbs')
+
+    // When they first go to the url/webpage
+    response.render('index.hbs');
+
+
+    // When they make a request to the server (this file!)
+    request.render('index.hbs')
+
 })
-
-
-app.post('/saveStudent', function(request, response) {
-    var id = request.body.id;
-    var name = request.body.name;
-    var email = request.body.email;
-    // response.send(id + ' ' + name + ' ' + email);
-
-    var db = utils.getDb();
-    db.collection('students').insertOne({
-        id: id,
-        name: name,
-        email: email
-        }, (err, result) => {
-        if (err) {
-            response.send('Unable to insert student');
-        }
-        response.send(JSON.stringify(result.ops, undefined, 2));
-    });
-});
-
-app.put('/updateStudent/:name', function(request, response) {
-    var db = utils.getDb();
-    var id = request.body.id;
-    var name = request.body.name;
-    var email = request.body.email;
-    db.collection('students').findOneAndUpdate({name: request.params.name}, {'$set': {'name': name, 'id': id, 'email': email}}, (err, item) => {
-        if (item['value'] == null) {
-            response.send(`Cannot find the document with the name of ${request.params.name}`);
-            console.log(`Updated: Cannot find ${request.params.name}`)
-        } else {
-            response.send(`Updated the document ${request.params.name} in the collection`);
-            console.log(`Updated: ${request.params.name}`);
-        }
-    });
-});
-
-app.delete('/deleteStudent/:name', function(request, response) {
-    var db = utils.getDb();
-    var name = request.params.name;
-    db.collection('students').findOneAndDelete({name: name}, (err, item) => {
-        if (item['value'] == null) {
-            response.send(`Cannot find document with the name of ${name}`);
-            console.log(`Deleted: Cannot find ${name}`);
-        } else {
-            response.send(`Deleted the document ${name} from the colleciton`);  
-            console.log(`Deleted: ${name}`);
-        }
-    });
-});
-
-app.get('/getStudent/:name', function(request, response) {
-    var db = utils.getDb();
-    var name = request.params.name;
-    db.collection('students').find({name: name}).toArray((err, items) => {
-        response.send(items);
-    });
-});
-
-app.get('/all', function(request, response) {
-    var db = utils.getDb();
-    db.collection('students').find().toArray((err, items) => {
-        response.send(items);
-    });
-    
-});
 
 app.listen(port, () => {
     console.log('Server is up and running');
