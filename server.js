04cf8ea4936d1c32c146cb25f5606537ecc11337
Standard <josephgotnco@gmail.com>
2019-04-17 08:36:45 -0700

    Navbar was added

--- server.js
+++ server.js
@@ -1,29 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');  
+const hbs = require('hbs');
 
-var utils = require('./utils');
 var app = express();
+
+// Port
 const port = process.env.PORT || 8080;
-app.set('views', __dirname)
-app.use(express.static(__dirname))
+
+// I don't know what this is for but whatever!
+app.use(express.static(__dirname));
+
+// Web Pages
+app.set('views', __dirname);
+
+// Partials
+hbs.registerPartials(__dirname + '/partials');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: false
 }));
 
 app.get('/', (request, response) => {
-
-    // When they first go to the url/webpage
-    response.render('index.hbs');
-
-
     // When they make a request to the server (this file!)
-    request.render('index.hbs')
+    response.render('index.hbs');
+});
 
-})
+app.get('/pictures', (request, response) => {
+    // When they make a request to the server (this file!)
+    response.render('index.hbs');
+});
+
+app.get('/weather', (request, response) => {
+    // When they make a request to the server (this file!)
+    response.render('index.hbs');
+});
+
+// app.post('/', (request, response) => {
+//     response.render('index.hbs', {
+//         returned_data: request.body["data"]
+//     })
+// })
 
 app.listen(port, () => {
     console.log('Server is up and running');
-    // utils.init()
 });
