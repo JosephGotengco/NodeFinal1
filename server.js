2255ff50d7f8e640b01d35369476c653505ebcbe
Standard <josephgotnco@gmail.com>
2019-04-17 11:49:56 -0700

    Final

--- server.js
+++ server.js
@@ -30,31 +30,39 @@ app.get('/', (request, response) => {
     });
 });
 
-app.get('/pictures', (request, response) => {
+app.get('/nasa', (request, response) => {
     // Gallery Get
-    response.render('pictures.hbs', {
+    response.render('nasa.hbs', {
         title: "Gallery"
     });
 });
 
-app.post('/pictures', async(request, response) => {
+app.post('/nasa', async(request, response) => {
     // Gallery Post
     console.log(request.body);
     var query = request.body["query"];
-    var url = `https://pixabay.com/api/?key=12227711-2b8be503764e201cc3222631d&q=${query}&image_type=photo`;
-    var image = await axios.get(url);
-    var images = image.data.hits;
-    query_image = images[0]["largeImageURL"];
-    console.log(query_image);
-    response.render('pictures.hbs', {
+    var url = `https://images-api.nasa.gov/search?q=${query}`;
+    var json = await axios.get(url);
+    var json = json.data.collection.items[4].href
+    console.log(json);
+    var gallery = await axios.get(json)
+    console.log(gallery.data)
+    // var gallery = await axios.get(gallery.data)
+    // console.log(gallery.data)
+    response.render('nasa.hbs', {
         title: "Gallery",
-        returned_image: query_image
+        image1: gallery.data[0],
+        image2: gallery.data[1],
+        image3: gallery.data[2],
+        // image4: gallery.data[3],
+        // image5: gallery.data[4],
+        // image6: gallery.data[5]
     })
 });
 
-app.get('/weather', (request, response) => {
+app.get('/deck', (request, response) => {
     // When they make a request to the server (this file!)
-    response.render('weather.hbs', {
+    response.render('deck.hbs', {
         title: "Forecast"
     });
 });
